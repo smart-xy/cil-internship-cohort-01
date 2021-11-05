92449b8d2f8970a0679620f78800dbd49b80d845
smart-xy <smartxysmart@gmail.com>
2021-11-05 13:04:08 +0100

    Software development process

--- Smart Nwamadu/JsAPI.js
+++ Smart Nwamadu/JsAPI.js
@@ -1,7 +1,7 @@
-# USING THE FETCH API
+//using fetch 
 
 const userAction = async () => {
-  const response = await fetch('http://example.com/movies.json');
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
   const myJson = await response.json(); //extract JSON from the http response
   // do something with myJson
 }
